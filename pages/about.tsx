7ed7f966d15e0015dde8ce5f79ea84702187ab30
Hoang Bui <jasonbui692@gmail.com>
2022-09-01 22:56:07 +0700

    b8 4

--- pages/about.tsx
+++ pages/about.tsx
@@ -20,6 +20,7 @@ export default function AboutPage(props: AboutPageProps) {
   console.log('About query lala111:  11asdasdadasas11', router.query);
   console.log('About query lala111:  11asdasdadasas11', router.query);
   console.log('About query lala111:  11asdasdadasas11', router.query);
+  console.log('About query lala111:  11asdasdadasas11', router.query);
 
   const page = router.query?.page;
   console.log('first4');
