e0b50671c0a94b999a79045c29a2af4fd72a1b84
Hoang Bui <jasonbui692@gmail.com>
2022-09-01 21:08:44 +0700

    push 2

--- pages/about.tsx
+++ pages/about.tsx
@@ -12,7 +12,7 @@ export default function AboutPage(props: AboutPageProps) {
   const router = useRouter();
   const [postList, setPostList] = useState([]);
 
-  console.log('About query nice: ', router.query);
+  console.log('About query nice 1: ', router.query);
   const page = router.query?.page;
 
   useEffect(() => {
